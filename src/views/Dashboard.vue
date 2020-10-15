deafa495db6bca406fa4794a7301b242a09b9ce2
seth <seth@sethpainter.com>
2020-10-15 06:45:36 +0000

    The user asociated with the app is now displayed.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,21 +1,28 @@
 <template>
 	<main>
 		<h1>Dashboard</h1>
-		<div>{{ JSON.stringify(userData) }}</div>
+		<div v-if="github_profile" id="user">
+			<img :src="github_profile.avatar_url" alt="">
+			<p>{{github_profile.login}}</p>
+		</div>
+		<div v-if="userData">{{ userData }}</div>
 	</main>
 </template>
 
 <script>
-let userData = 1;
+let userData = null, github_profile;
+
 export default {
 	name: 'Dashboard',
-	created() {
-		fetch(`${process.env.VUE_APP_SERVER_BASE_URL}/user/${localStorage.getItem('userID')}`)
-			.then(res => res.json())
-			.then(data => this.userData = data);
+	async created() {
+		// const req = ;
+		this.userData = await (
+			await fetch(`${process.env.VUE_APP_SERVER_BASE_URL}/user/${localStorage.getItem('userID')}`)).json();
+		this.github_profile = await (
+			await fetch('https://api.github.com/user', {headers: {'authorization': `token ${this.userData.access_token}`}})).json();
 	},
 	data() {
-		return {userData};
+		return {userData, github_profile};
 	}
 };
 </script>
