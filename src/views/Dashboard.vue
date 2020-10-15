6e82dadf2c14c27e59765a4be2bad1e41e6db39a
seth <seth@sethpainter.com>
2020-10-15 07:44:18 +0000

    Included a libarary to display existing contribution history

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,25 +1,32 @@
 <template>
 	<main>
 		<h1>Dashboard</h1>
-		<div v-if="github_profile" id="user">
+		<div id="user">
 			<img :src="github_profile.avatar_url" alt="">
 			<p>{{github_profile.login}}</p>
+			<div id="calander" ref="calander">
+
+			</div>
 		</div>
 		<div v-if="userData">{{ userData }}</div>
 	</main>
 </template>
 
 <script>
+import GithubCalander from 'github-calendar';
+
 let userData = null, github_profile;
 
 export default {
 	name: 'Dashboard',
 	async created() {
-		// const req = ;
 		this.userData = await (
 			await fetch(`${process.env.VUE_APP_SERVER_BASE_URL}/user/${localStorage.getItem('userID')}`)).json();
 		this.github_profile = await (
 			await fetch('https://api.github.com/user', {headers: {'authorization': `token ${this.userData.access_token}`}})).json();
+	},
+	mounted() {
+		GithubCalander('#calander', this.github_profile.login);
 	},
 	data() {
 		return {userData, github_profile};
